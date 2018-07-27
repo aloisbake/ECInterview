0629541f04de8acffac6282c2c7cbed7c36dc1bc
aloisbake <aloisbake@gmail.com>
2018-07-27 16:15:23 +0200

    Project commit

---
+++ Question1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********    Password Validator Appliccation   ************");
+            bool result = false;
+            var ErrorMsg = string.Empty;
+
+            while (!result)
+            {
+                Console.WriteLine("Enter Passord");
+                var password = Console.ReadLine();
+                result = ValidatePassword(password, out ErrorMsg);
+                if (result)
+                {
+                    Console.WriteLine("Congradulations !!!! Your password meets the basic requirements");
+                }
+                else {
+                    Console.WriteLine(ErrorMsg);
+                }
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static bool ValidatePassword(string password, out string ErrorMsg)
+        {
+            ErrorMsg = string.Empty;
+            Regex checkLength = new Regex(@"(\s*(\S)\s*){6,}");
+            Regex hasUpperCase = new Regex(@"[A-Z]+");
+            Regex hasLowercase = new Regex(@"[a-z]+");
+            Regex hasSpecialCharacters = new Regex(@"[!@#$%^&*()-+]");
+            Regex hasNumber = new Regex(@"[0-9]+");
+
+            if (!checkLength.IsMatch(password))
+            {
+                ErrorMsg = "Password length shoud be at least 6 characters";
+                return false;
+            }
+            else if (!hasUpperCase.IsMatch(password))
+            {
+                ErrorMsg = "Password should contain at least one uppercase English character";
+                return false;
+            }
+            else if (!hasLowercase.IsMatch(password))
+            {
+                ErrorMsg = "Password should contain at least one lowercase English character";
+                return false;
+            }
+            else if (!hasNumber.IsMatch(password))
+            {
+                ErrorMsg = "Password should contain at least a number";
+                return false;
+            }
+            else if (!hasSpecialCharacters.IsMatch(password))
+            {
+                ErrorMsg = "Password should contain at least a special character in the following list !@#$%^&*()-+";
+                return false;
+            }
+            else
+            {           
+                return true;
+            }
+        }
+    }
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********* Frequency Application ************");
+            List<int> firstIds = new List<int>();
+            List<int> secondIds = new List<int>();
+            List<int> missingIds = new List<int>();
+
+
+            Console.WriteLine("Enter the length of your first list");
+            var firstArrayLength = Console.ReadLine();
+            Console.WriteLine("Enter your list of numbers separated by a space");
+            var firstArrayString = Console.ReadLine();           
+
+            string[] firstString = firstArrayString.Split(' ');
+
+            foreach (var item in firstString) {
+                firstIds.Add(int.Parse(item));
+            }
+
+            var firstListFrequency = firstIds.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            Console.WriteLine("Enter the length of your second list");
+            var secondArrayLength = Console.ReadLine();
+            Console.WriteLine("Enter your list of numbers separated by a space");
+            var secondArrayString = Console.ReadLine();
+            string[] secondString = secondArrayString.Split(' ');
+
+            foreach (var item in secondString)
+            {
+                secondIds.Add(int.Parse(item));
+            }
+
+            var maxNum = secondIds.Max();
+            var minNum = secondIds.Min();
+
+            if (maxNum - minNum <= 100 && minNum > 0)
+            {
+                var secondListFrequency = secondIds.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+                foreach (var item in firstListFrequency)
+                {
+                    if (secondListFrequency.ContainsKey(item.Key))
+                    {
+                        var second = secondListFrequency.Where(x => x.Key == item.Key).Select(x => x.Value);
+
+                        if (item.Value.ToString() != second.ToString())
+                        {
+                            missingIds.Add(Convert.ToInt32(item.Key));
+                        }
+                    }
+                }
+                Console.WriteLine("Missing numbers are below : ");
+                foreach (var item in missingIds)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Second list of integers need to have a diffrence not more than 100 between the max and min numbers");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
